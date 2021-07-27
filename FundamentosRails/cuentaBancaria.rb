2051e86caf3b74689ab4d33b3fd209bde238a677
adriana8701 <abk.cardenas@gmail.com>
2021-07-27 08:40:59 -0400

    mas rubyrails

---
+++ FundamentosRails/cuentaBancaria.rb
@@ -0,0 +1,12 @@
+
+#CuentaBancaria debe tener un método que devuelva el saldo de la cuenta corriente del usuario. 
+#CuentaBancaria debe tener un método que devuelva el saldo de la cuenta de ahorro del usuario. 
+#CuentaBancaria debe permitir que un usuario deposite dinero en su cuenta de ahorro o corriente.
+#CuentaBancaria debe permitir a un usuario retirar dinero de una de sus cuentas, devolver un error si no hay fondos suficientes. 
+#CuentaBancaria debe permitir al usuario ver la cantidad total de dinero que tiene en el banco.
+#CuentaBancaria debe rastrear cuántas cuentas tiene actualmente el banco. 
+#Agregue un atributo tasa_interes al que el usuario no pueda acceder.
+#Ajústelo a 0.01.  
+#CuentaBancaria debe tener un método llamado informacion_cuenta que muestre el número de cuenta del usuario,
+#el dinero total, el saldo de la cuenta corriente, el saldo de la cuenta de ahorros y la tasa de interés. 
+#Un usuario no puede establecer ningún atributo de la clase CuentaBancaria.
